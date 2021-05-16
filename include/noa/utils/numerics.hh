78cfaff59b68066bea5c94088fe7f8a78b5f16ac
Roland Grinis <roland.grinis@grinisrit.com>
2021-05-16 18:55:28 +0100

    hot fix bug in numerics.hh

--- include/noa/utils/numerics.hh
+++ include/noa/utils/numerics.hh
@@ -32,112 +32,107 @@
 
 #include <optional>
 
-namespace noa::utils::numerics
-{
+namespace noa::utils::numerics {
 
     // https://pomax.github.io/bezierinfo/legendre-gauss.html
-    template <typename Dtype, typename Function>
+    template<typename Dtype, typename Function>
     inline Dtype legendre_gaussian_quadrature(const Dtype &lower_bound,
                                               const Dtype &upper_bound,
                                               const Function &function,
                                               const int32_t min_points,
                                               const int32_t order,
                                               const Dtype *abscissa,
-                                              const Dtype *weight)
-    {
+                                              const Dtype *weight) {
         const int32_t n_itv = (min_points + order - 1) / order;
         const Dtype h = (upper_bound - lower_bound) / n_itv;
         const int32_t N = n_itv * order;
         Dtype res = 0;
         Dtype x0 = lower_bound;
-        for (int32_t i = 0; i < N; i++)
-        {
+        for (int32_t i = 0; i < N; i++) {
             int32_t j = i % order;
             res += function(x0 + h * ((i / order) + abscissa[j])) * h * weight[j];
         }
         return res;
     }
 
-    template <typename Dtype, typename Function>
+    template<typename Dtype, typename Function>
     inline Dtype quadrature6(const Dtype &lower_bound,
                              const Dtype &upper_bound,
                              const Function &function,
-                             const int32_t min_points = 1)
-    {
+                             const int32_t min_points = 1) {
         constexpr int32_t N_GQ = 6;
-        const Dtype xGQ[N_GQ] = {0.03376524, 0.16939531, 0.38069041,
-                                 0.61930959, 0.83060469, 0.96623476};
-        const Dtype wGQ[N_GQ] = {0.08566225, 0.18038079, 0.23395697,
-                                 0.23395697, 0.18038079, 0.08566225};
+        const Dtype xGQ[N_GQ] = {(Dtype) 0.03376524, (Dtype) 0.16939531, (Dtype) 0.38069041,
+                                 (Dtype) 0.61930959, (Dtype) 0.83060469, (Dtype) 0.96623476};
+        const Dtype wGQ[N_GQ] = {(Dtype) 0.08566225, (Dtype) 0.18038079, (Dtype) 0.23395697,
+                                 (Dtype) 0.23395697, (Dtype) 0.18038079, (Dtype) 0.08566225};
 
         return legendre_gaussian_quadrature<Dtype>(
-            lower_bound,
-            upper_bound,
-            function,
-            min_points,
-            N_GQ, xGQ, wGQ);
+                lower_bound,
+                upper_bound,
+                function,
+                min_points,
+                N_GQ, xGQ, wGQ);
     }
 
-    template <typename Dtype, typename Function>
+    template<typename Dtype, typename Function>
     inline Dtype quadrature8(const Dtype &lower_bound,
                              const Dtype &upper_bound,
                              const Function &function,
-                             const int32_t min_points = 1)
-    {
+                             const int32_t min_points = 1) {
         constexpr int32_t N_GQ = 8;
-        const Dtype xGQ[N_GQ] = {0.01985507, 0.10166676, 0.2372338,
-                                 0.40828268, 0.59171732, 0.7627662, 0.89833324, 0.98014493};
-        const Dtype wGQ[N_GQ] = {0.05061427, 0.11119052, 0.15685332,
-                                 0.18134189, 0.18134189, 0.15685332, 0.11119052, 0.05061427};
+        const Dtype xGQ[N_GQ] = {(Dtype) 0.01985507, (Dtype) 0.10166676, (Dtype) 0.2372338,
+                                 (Dtype) 0.40828268, (Dtype) 0.59171732, (Dtype) 0.7627662,
+                                 (Dtype) 0.89833324, (Dtype) 0.98014493};
+        const Dtype wGQ[N_GQ] = {(Dtype) 0.05061427, (Dtype) 0.11119052, (Dtype) 0.15685332,
+                                 (Dtype) 0.18134189, (Dtype) 0.18134189, (Dtype) 0.15685332,
+                                 (Dtype) 0.11119052, (Dtype) 0.05061427};
 
         return legendre_gaussian_quadrature(
-            lower_bound,
-            upper_bound,
-            function,
-            min_points,
-            N_GQ, xGQ, wGQ);
+                lower_bound,
+                upper_bound,
+                function,
+                min_points,
+                N_GQ, xGQ, wGQ);
     }
 
-    template <typename Dtype, typename Function>
+    template<typename Dtype, typename Function>
     inline Dtype quadrature9(const Dtype &lower_bound,
                              const Dtype &upper_bound,
                              const Function &function,
-                             const int32_t min_points = 1)
-    {
+                             const int32_t min_points = 1) {
         constexpr int32_t N_GQ = 9;
-        const Dtype xGQ[N_GQ] = {0.0000000000000000, -0.8360311073266358,
-                                 0.8360311073266358, -0.9681602395076261, 0.9681602395076261,
-                                 -0.3242534234038089, 0.3242534234038089, -0.6133714327005904,
-                                 0.6133714327005904};
-        const Dtype wGQ[N_GQ] = {0.3302393550012598, 0.1806481606948574,
-                                 0.1806481606948574, 0.0812743883615744, 0.0812743883615744,
-                                 0.3123470770400029, 0.3123470770400029, 0.2606106964029354,
-                                 0.2606106964029354};
+        const Dtype xGQ[N_GQ] = {(Dtype) 0.0000000000000000, (Dtype) -0.8360311073266358,
+                                 (Dtype) 0.8360311073266358, (Dtype) -0.9681602395076261, (Dtype) 0.9681602395076261,
+                                 (Dtype) -0.3242534234038089, (Dtype) 0.3242534234038089, (Dtype) -0.6133714327005904,
+                                 (Dtype) 0.6133714327005904};
+        const Dtype wGQ[N_GQ] = {(Dtype) 0.3302393550012598, (Dtype) 0.1806481606948574,
+                                 (Dtype) 0.1806481606948574, (Dtype) 0.0812743883615744, (Dtype) 0.0812743883615744,
+                                 (Dtype) 0.3123470770400029, (Dtype) 0.3123470770400029, (Dtype) 0.2606106964029354,
+                                 (Dtype) 0.2606106964029354};
 
         return legendre_gaussian_quadrature(
-            lower_bound,
-            upper_bound,
-            function,
-            min_points,
-            N_GQ, xGQ, wGQ);
+                lower_bound,
+                upper_bound,
+                function,
+                min_points,
+                N_GQ, xGQ, wGQ);
     }
 
     //https://en.wikipedia.org/wiki/Ridders%27_method
-    template <typename Dtype, typename Function>
+    template<typename Dtype, typename Function>
     inline std::optional<Dtype> ridders_root(
-        Dtype xa, //The lower bound of the search interval.
-        Dtype xb, //The upper bound of the search interval.
-        //The objective function to resolve.
-        const Function &function,
-        // The initial value at *a*,
-        const std::optional<Dtype> &fa_ = std::nullopt,
-        // The initial value at *b*
-        const std::optional<Dtype> &fb_ = std::nullopt,
-        //The absolute & relative tolerance on the root value.
-        const Dtype &xtol = TOLERANCE,
-        const Dtype &rtol = TOLERANCE,
-        const int32_t max_iter = 100)
-    {
+            Dtype xa, //The lower bound of the search interval.
+            Dtype xb, //The upper bound of the search interval.
+            //The objective function to resolve.
+            const Function &function,
+            // The initial value at *a*,
+            const std::optional<Dtype> &fa_ = std::nullopt,
+            // The initial value at *b*
+            const std::optional<Dtype> &fb_ = std::nullopt,
+            //The absolute & relative tolerance on the root value.
+            const Dtype &xtol = TOLERANCE,
+            const Dtype &rtol = TOLERANCE,
+            const int32_t max_iter = 100) {
         //  Check the initial values
         Dtype fa = (fa_.has_value()) ? fa_.value() : function(xa);
         Dtype fb = (fb_.has_value()) ? fb_.value() : function(xb);
@@ -151,12 +146,11 @@ namespace noa::utils::numerics
 
         // Set the tolerance for the root finding
         const Dtype tol =
-            xtol + rtol * std::min(std::abs(xa), std::abs(xb));
+                xtol + rtol * std::min(std::abs(xa), std::abs(xb));
 
         // Do the bracketing using Ridder's update rule.
         Dtype xn = 0.;
-        for (int32_t i = 0; i < max_iter; i++)
-        {
+        for (int32_t i = 0; i < max_iter; i++) {
             Dtype dm = 0.5 * (xb - xa);
             const Dtype xm = xa + dm;
             const Dtype fm = function(xm);
@@ -169,20 +163,15 @@ namespace noa::utils::numerics
                 dm = dn;
             xn = xm - sgn * dm;
             const Dtype fn = function(xn);
-            if (fn * fm < 0.0)
-            {
+            if (fn * fm < 0.0) {
                 xa = xn;
                 fa = fn;
                 xb = xm;
                 fb = fm;
-            }
-            else if (fn * fa < 0.0)
-            {
+            } else if (fn * fa < 0.0) {
                 xb = xn;
                 fb = fn;
-            }
-            else
-            {
+            } else {
                 xa = xn;
                 fa = fn;
             }
