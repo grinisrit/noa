018810352b6163382e9fa0e0f9d341c7335d3eec
Roland Grinis <roland.grinis@grinisrit.com>
2021-06-29 19:21:37 +0100

    muons dcs tutorial

--- docs/pms/muons.cc
+++ docs/pms/muons.cc
@@ -14,6 +14,27 @@ inline Tensor bremsstrahlung(Tensor kinetic_energies, Tensor recoil_energies) {
     return result;
 }
 
+inline Tensor pair_production(Tensor kinetic_energies, Tensor recoil_energies) {
+    const auto result = torch::zeros_like(kinetic_energies);
+    dcs::vmap<Scalar>(dcs::pumas::pair_production)(
+            result, kinetic_energies, recoil_energies, STANDARD_ROCK, MUON_MASS);
+    return result;
+}
+
+inline Tensor photonuclear(Tensor kinetic_energies, Tensor recoil_energies) {
+    const auto result = torch::zeros_like(kinetic_energies);
+    dcs::vmap<Scalar>(dcs::pumas::photonuclear)(
+            result, kinetic_energies, recoil_energies, STANDARD_ROCK, MUON_MASS);
+    return result;
+}
+
+inline Tensor ionisation(Tensor kinetic_energies, Tensor recoil_energies) {
+    const auto result = torch::zeros_like(kinetic_energies);
+    dcs::vmap<Scalar>(dcs::pumas::ionisation)(
+            result, kinetic_energies, recoil_energies, STANDARD_ROCK, MUON_MASS);
+    return result;
+}
+
 inline void serialise(Tensor tensor, std::string path) {
     torch::save(tensor, path);
 }
@@ -22,6 +43,12 @@ inline void serialise(Tensor tensor, std::string path) {
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("bremsstrahlung", &bremsstrahlung, py::call_guard<py::gil_scoped_release>(),
           "Standard Rock Bremsstrahlung DCS for Muons");
+    m.def("pair_production", &pair_production, py::call_guard<py::gil_scoped_release>(),
+          "Standard Rock Pair production DCS for Muons");
+    m.def("photonuclear", &photonuclear, py::call_guard<py::gil_scoped_release>(),
+          "Standard Rock Photonuclear DCS for Muons");
+    m.def("ionisation", &ionisation, py::call_guard<py::gil_scoped_release>(),
+          "Standard Rock Ionisation DCS for Muons");
     m.def("serialise", &serialise, py::call_guard<py::gil_scoped_release>(),
           "Save tensor to disk");
 }
