11804a01a15a294e4006783bd65d07b43c57227e
Roland Grinis <roland.grinis@grinisrit.com>
2022-01-27 18:33:06 +0000

    loading binary model

--- src/noa/pms/pumas.hh
+++ src/noa/pms/pumas.hh
@@ -37,7 +37,7 @@ namespace noa::pms::pumas {
         using PhysicsModelOpt = std::optional<PhysicsModel>;
         using MDFPath = utils::Path;
         using DEDXPath = utils::Path;
-        using DumpPath = utils::Path;
+        using BinaryPath = utils::Path;
 
         friend ParticleModel;
 
@@ -48,20 +48,37 @@ namespace noa::pms::pumas {
 
         inline utils::Status create_physics(
                 const MDFPath &mdf_path,
-                const DEDXPath &dedx_path
-        ) {
-            const auto status =
-                    pumas_physics_create(&physics, particle, mdf_path.c_str(), dedx_path.c_str(), nullptr);
-            return status == PUMAS_RETURN_SUCCESS;
+                const DEDXPath &dedx_path) {
+            if (utils::check_path_exists(mdf_path) && utils::check_path_exists(dedx_path)) {
+                const auto status =
+                        pumas_physics_create(&physics, particle, mdf_path.c_str(), dedx_path.c_str(), nullptr);
+                return status == PUMAS_RETURN_SUCCESS;
+            }
+            return false;
+        }
+
+        inline utils::Status load_physics(const BinaryPath &binary_path) {
+            if (utils::check_path_exists(binary_path)) {
+                auto handle = fopen(binary_path.c_str(), "rb");
+                if (handle != nullptr) {
+                    const auto status =
+                            pumas_physics_load(&physics, handle);
+                    fclose(handle);
+                    return status == PUMAS_RETURN_SUCCESS;
+                } else std::cerr << "Failed to open " << binary_path << "\n";
+            }
+            return false;
         }
 
     public:
 
-        PhysicsModel(const PhysicsModel &other) = delete;
+        PhysicsModel(
+                const PhysicsModel &other) = delete;
 
         auto &operator=(const PhysicsModel &other) = delete;
 
-        PhysicsModel(PhysicsModel &&other) noexcept
+        PhysicsModel(PhysicsModel &&other)
+        noexcept
                 : particle{other.particle}, physics{other.physics} {
             other.physics = nullptr;
         }
@@ -78,14 +95,14 @@ namespace noa::pms::pumas {
             physics = nullptr;
         }
 
-        inline utils::Status save_binary(const DumpPath &dump_path) const {
+        inline utils::Status save_binary(const BinaryPath &binary_path) const {
 
-            auto handle = fopen(dump_path.c_str(), "wb");
+            auto handle = fopen(binary_path.c_str(), "wb");
             if (handle != nullptr) {
                 pumas_physics_dump(physics, handle);
                 fclose(handle);
             } else {
-                std::cerr << "Failed to open " << dump_path << "\n";
+                std::cerr << "Failed to open " << binary_path << "\n";
                 return false;
             }
             return true;
@@ -96,6 +113,12 @@ namespace noa::pms::pumas {
                 const DEDXPath &dedx_path) {
             auto model = ParticleModel{};
             const auto status = model.create_physics(mdf_path, dedx_path);
+            return status ? PhysicsModelOpt{std::move(model)} : PhysicsModelOpt{};
+        }
+
+        inline static PhysicsModelOpt load_from_binary(const BinaryPath &binary_path) {
+            auto model = ParticleModel{};
+            const auto status = model.load_physics(binary_path);
             return status ? PhysicsModelOpt{std::move(model)} : PhysicsModelOpt{};
         }
 
--- test/pms/muon-model.cc
+++ test/pms/muon-model.cc
@@ -23,8 +23,11 @@ auto main(int argc, char **argv) -> int {
     const auto mdf_file = material_dir / "mdf" / "examples" / "standard.xml";
     const auto dedx_dir = material_dir / "dedx";
 
-    const auto modelOpt = pms::pumas::MuonModel::load_from_mdf(mdf_file, dedx_dir);
-    if (!modelOpt.has_value()) return 1;
+    auto modelOpt = pms::pumas::MuonModel::load_from_binary(FLAGS_binary_model);
+    if (!modelOpt.has_value()){
+        modelOpt = pms::pumas::MuonModel::load_from_mdf(mdf_file, dedx_dir);
+        if (!modelOpt.has_value()) return 1;
+    }
 
     const auto &model = modelOpt.value();
     model.save_binary(FLAGS_binary_model);
