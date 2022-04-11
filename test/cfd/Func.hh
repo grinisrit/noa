6b806476ffa8fdaea6722b503d4360ea292e4d63
GregTheMadMonk <yagreg7@gmail.com>
2022-04-11 11:51:56 +0300

    Make proper use of enable_if/add explict `inline`

--- test/cfd/Func.hh
+++ test/cfd/Func.hh
@@ -7,22 +7,24 @@ using TopoTriangle = TNL::Meshes::Topologies::Triangle;
 // TODO: Make all the parameters uniform
 // TODO: Passing alpha_i assumes that α_i = α_j which may not be true for some topologies
 
+template <typename CellTopology, typename RequiredTopology, typename Type>
+using EnableIfSame = std::enable_if_t<std::is_same_v<CellTopology, RequiredTopology>, Type>;
+
 /* ----- MHFE ----- */
 struct MHFEDelta {
-	template <typename CellTopology, typename Real,
-		 std::enable_if_t<std::is_same<CellTopology, TopoTriangle>::value, bool> = true>
-	static Real get(const Real& alpha_i, const Real& alpha,
-			const Real& lambda, const Real& beta,
-			const Real& a, const Real& c, const Real& l,
-			const Real& mes, const Real& tau,
-			const Real& BinvEl) {
+	template <typename CellTopology, typename Real>
+	inline static EnableIfSame<CellTopology, TopoTriangle, Real> get(const Real& alpha_i, const Real& alpha,
+								const Real& lambda, const Real& beta,
+								const Real& a, const Real& c, const Real& l,
+								const Real& mes, const Real& tau,
+								const Real& BinvEl) {
 		return a * (BinvEl - a / l / l / beta);
 	}
 };
 
 struct MHFELumping {
 	template <typename CellTopology, typename Real>
-	static Real get(const Real& alpha_i, const Real& alpha,
+	inline static Real get(const Real& alpha_i, const Real& alpha,
 			const Real& lambda, const Real& beta,
 			const Real& a, const Real& c, const Real& l,
 			const Real& mes, const Real& tau,
@@ -32,50 +34,46 @@ struct MHFELumping {
 };
 
 struct MHFERight {
-	template <typename CellTopology, typename Real,
-		 std::enable_if_t<std::is_same<CellTopology, TopoTriangle>::value, bool> = true>
-	static Real get(const Real& alpha_i, const Real& alpha,
-			const Real& lambda, const Real& beta,
-			const Real& a, const Real& c, const Real& l,
-			const Real& mes, const Real& tau,
-			const Real& prevVal, const Real& edgeVal) {
+	template <typename CellTopology, typename Real>
+	inline static EnableIfSame<CellTopology, TopoTriangle, Real> get(const Real& alpha_i, const Real& alpha,
+								const Real& lambda, const Real& beta,
+								const Real& a, const Real& c, const Real& l,
+								const Real& mes, const Real& tau,
+								const Real& prevVal, const Real& edgeVal) {
 		return a * lambda / l / beta * prevVal;
 	}
 };
 
 /* ----- Lumped MHFE ----- */
 struct LMHFEDelta {
-	template <typename CellTopology, typename Real,
-		 std::enable_if_t<std::is_same<CellTopology, TopoTriangle>::value, bool> = true>
-	static Real get(const Real& alpha_i, const Real& alpha,
-			const Real& lambda, const Real& beta,
-			const Real& a, const Real& c, const Real& l,
-			const Real& mes, const Real& tau,
-			const Real& BinvEl) {
+	template <typename CellTopology, typename Real>
+	inline static EnableIfSame<CellTopology, TopoTriangle, Real> get(const Real& alpha_i, const Real& alpha,
+								const Real& lambda, const Real& beta,
+								const Real& a, const Real& c, const Real& l,
+								const Real& mes, const Real& tau,
+								const Real& BinvEl) {
 		return a * (BinvEl - alpha_i * alpha_i / alpha);
 	}
 };
 
 struct LMHFELumping {
-	template <typename CellTopology, typename Real,
-		 std::enable_if_t<std::is_same<CellTopology, TopoTriangle>::value, bool> = true>
-	static Real get(const Real& alpha_i, const Real& alpha,
-			const Real& lambda, const Real& beta,
-			const Real& a, const Real& c, const Real& l,
-			const Real& mes, const Real& tau,
-			const Real& BinvEl) {
+	template <typename CellTopology, typename Real>
+	inline static EnableIfSame<CellTopology, TopoTriangle, Real> get(const Real& alpha_i, const Real& alpha,
+								const Real& lambda, const Real& beta,
+								const Real& a, const Real& c, const Real& l,
+								const Real& mes, const Real& tau,
+								const Real& BinvEl) {
 		return c * mes / 3.0 / tau;
 	}
 };
 
 struct LMHFERight {
-	template <typename CellTopology, typename Real,
-		 std::enable_if_t<std::is_same<CellTopology, TopoTriangle>::value, bool> = true>
-	static Real get(const Real& alpha_i, const Real& alpha,
-			const Real& lambda, const Real& beta,
-			const Real& a, const Real& c, const Real& l,
-			const Real& mes, const Real& tau,
-			const Real& prevVal, const Real& edgeVal) {
+	template <typename CellTopology, typename Real>
+	inline static EnableIfSame<CellTopology, TopoTriangle, Real> get(const Real& alpha_i, const Real& alpha,
+								const Real& lambda, const Real& beta,
+								const Real& a, const Real& c, const Real& l,
+								const Real& mes, const Real& tau,
+								const Real& prevVal, const Real& edgeVal) {
 		return c * mes * edgeVal / 3.0 / tau;
 	}
 };
