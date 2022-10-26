16e4ca5ae237e432fec8dfdedcc782d7406eb3f5
molozey <lipskii.as@phystech.edu>
2022-10-26 04:10:08 +0300

    Created: async request for batch of downloading market_data

--- HestonModel/OptionMarketScrapper/Scrapper.py
+++ HestonModel/OptionMarketScrapper/Scrapper.py
@@ -24,27 +24,33 @@ def send_request(msg, show_answer=False) -> json:
 
 
 def send_batch_of_requests(msg_list, show_answer=False) -> json:
-    L = list()
-    async def circle(OBJECT):
+    return_list = list()
+
+    async def circle(mutable_list):
         messages = [call_api(json.dumps(msg)) for msg in msg_list]
         # Schedule three calls *concurrently*:
-        OBJECT += await asyncio.gather(*messages)
+        mutable_list += await asyncio.gather(*messages)
 
-    asyncio.run(circle(L))
-    pprint(L)
+    asyncio.run(circle(return_list))
+    if show_answer:
+        pprint(return_list)
+
+    return return_list
 
 
-def test_multple_downloading():
-    names = ["BTC-29SEP23-10000-C", "BTC-30DEC22-10000-C", "BTC-30JUN23-10000-C",
-             "BTC-31MAR23-10000-C", "BTC-25NOV22-12000-C", "BTC-25NOV22-14000-C",
-             "BTC-28OCT22-14000-C", "BTC-29SEP23-14000-C"]
+def get_multiple_ticker_request():
+    # names = ["BTC-29SEP23-10000-C", "BTC-30DEC22-10000-C", "BTC-30JUN23-10000-C",
+    #          "BTC-31MAR23-10000-C", "BTC-25NOV22-12000-C", "BTC-25NOV22-14000-C",
+    #          "BTC-28OCT22-14000-C", "BTC-29SEP23-14000-C"]
+    names = ["BTC-23OCT22-15000-C"]
     messages_list = list(map(get_ticker_by_instrument_request, names))
     send_batch_of_requests(messages_list)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     from AvailableRequests import get_instruments_by_currency_request, get_ticker_by_instrument_request
-    test_multple_downloading()
+
+    get_multiple_ticker_request()
 
     # answer = send_request(get_instruments_by_currency_request(AvailableCurrencies.Currency.BITCOIN,
     #                                                           AvailableInstrumentType.InstrumentType.OPTION))
