8df8c77dce4ab5e06986aec39755c7d8fd21449c
GregTheMadMonk <yagreg7@gmail.com>
2022-12-10 14:15:01 +0300

    Fix: right-hand side of `while` loop condition not evaluated sometimes

--- test/unit/test-domain.hh
+++ test/unit/test-domain.hh
@@ -67,7 +67,7 @@ TEST(DOMAIN, SaveLoadDomain) {
 	std::ifstream d1file(test_domain_file, std::ios::binary), d2file(test_domain_file_2, std::ios::binary);
 	ASSERT_EQ(d1file.good() && d2file.good(), true);
 	char c1, c2;
-	while (d1file.read(&c1, 1) && d2file.read(&c2, 1))
+	while (bool(d1file.read(&c1, 1)) & bool(d2file.read(&c2, 1))) // & instead of && to ensure both sides get executed
 		ASSERT_EQ(c1, c2);
 	ASSERT_EQ(d1file.eof() && d2file.eof(), true); // Check if reached EOF in both files
 }
