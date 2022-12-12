e8fc2ba6fa7aeefa69b3f32a37e5de61bd3a47f7
mrFendel <novikov.ivan@phystech.edu>
2022-12-12 06:24:50 +0300

    Changes: - added sensitivities with finite differences for Heat Equation

--- docs/quant/heat_equation/HeatGrid.py
+++ docs/quant/heat_equation/HeatGrid.py
@@ -9,14 +9,19 @@ from typing import Dict, Optional
 class Mode(Enum):
     MAIN = 'main net'
     CASH = 'additional cashed net'
-    DIFF = 'sensitivities by finite differences'
+    DIFF_T = 'sensitivity by time via finite differences'
+    DIFF_X = 'sensitivity by x via finite differences'
+    DIFF_C = 'sensitivity by sigma coef via finite differences'
 
 
+# TODO: Add sensitivities by sigma via finite differences
 class HeatGrid:
     _net_mod_map: Dict[Mode, Optional[ndarray]] = {
         Mode.MAIN: None,
         Mode.CASH: None,
-        Mode.DIFF: None,
+        Mode.DIFF_T: None,
+        Mode.DIFF_X: None,
+        Mode.DIFF_C: None,
     }
 
     def __init__(self,
@@ -34,7 +39,6 @@ class HeatGrid:
         self.tGrid = np.linspace(0, tMax, tSteps + 1)
         self.dx = (xRight - xLeft) / xSteps
         self.dt = tMax / tSteps
-        self.lamda = self.dt / self.dx ** 2
 
     @property
     def net(self):
@@ -50,26 +54,41 @@ class HeatGrid:
     def backup(self):
         self._net_mod_map[Mode.CASH] = self.net
 
-    # TODO: Add sensitivities by t, x, sigma via finite differences
-
     def clear(self):
         self._net = np.zeros((self.xSteps + 1, self.tSteps + 1))
         for mod in Mode:
             self._net_mod_map[mod] = None
 
+    def diff_t(self):
+        self._net_mod_map[Mode.DIFF_T] = (self._net[:, 1:] - self.net[:, :-1]) / self.dt
+
+    def diff_x(self):
+        self._net_mod_map[Mode.DIFF_X] = (self._net[1:, :] - self.net[:-1, :]) / self.dx
+
+    def _get_mod_grid(self, mod):
+        match mod:
+            case Mode.DIFF_T:
+                return self.xGrid, self.tGrid[:-1]
+            case Mode.DIFF_X:
+                return self.xGrid[:-1], self.tGrid
+            case _:
+                return self.xGrid, self.tGrid
+
     def plot(self, slice_num=5, mod=Mode.MAIN):
         net = self._net_mod_map[mod]
-        indexes = np.linspace(0, self.tSteps - 1, slice_num)
+        x, t = self._get_mod_grid(mod)
+        indexes = np.linspace(0, len(t)-1, slice_num)
         plt.style.use("dark_background")
         plt.figure(figsize=(15, 8))
         for i in indexes:
-            plt.plot(self.xGrid, net[:, int(i)], label=f"t = {round(self.tGrid[int(i)], 2)}")
+            plt.plot(x, net[:, int(i)], label=f"t = {round(t[int(i)], 2)}")
         plt.legend()
         plt.show()
 
     def plot3D(self, mod=Mode.MAIN):
         net = self._net_mod_map[mod]
-        surface = go.Surface(z=net, x=self.tGrid, y=self.xGrid)
+        x, t = self._get_mod_grid(mod)
+        surface = go.Surface(z=net, x=t, y=x)
         fig = go.Figure(surface)
         fig.update_layout(title='V(S,t)', autosize=False, width=1200, height=800,
                           margin=dict(l=65, r=50, b=65, t=90))
--- docs/quant/heat_equation/HeatSolver.py
+++ docs/quant/heat_equation/HeatSolver.py
@@ -14,11 +14,14 @@ class HeatSolver(HeatGrid):
     def __init__(self,
                  xSteps: int,
                  tSteps: int,
+                 sigma=1,
                  xLeft=-3.0,
                  xRight=3.0,
                  tMax=1.0):
 
         super().__init__(xSteps, tSteps, xLeft, xRight, tMax)
+        self.lambda_ = sigma * self.dt / self.dx ** 2
+        # TODO: sigma == sigma(xx, tt)
 
     def setBounds(self):
         # boundary conditions
@@ -30,5 +33,14 @@ class HeatSolver(HeatGrid):
 
     def CN(self):
         self.setBounds()
-        self._net = crank_nickolson_scheme(self.net, self.lamda)
+        self._net = crank_nickolson_scheme(self.net, self.lambda_)
         self._net_mod_map[Mode.MAIN] = self.net
+
+    # TODO: manage with cashing and if before CN and without backup
+    def diff_sigma(self, shift_percent=0.001):
+        if self._net_mod_map[Mode.CASH] is None:
+            print('Please solvee CN and make backup (CASH is empty)')
+        else:
+            self._net_mod_map[Mode.DIFF_C] = \
+                (crank_nickolson_scheme(self.net, self.lambda_*(1 + shift_percent)) - self._net_mod_map[Mode.CASH]) / (shift_percent*self.lambda_)
+
