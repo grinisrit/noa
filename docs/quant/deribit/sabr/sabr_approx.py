0c84ece3970ae730a084dd2dd790a4fa96e5beca
Dmitriy Bazanov <zugzvangg1@gmail.com>
2023-03-16 22:58:37 +0300

    feat: Add sabr approximation implementation + jups

---
+++ docs/quant/deribit/sabr/sabr_approx.py
@@ -0,0 +1,437 @@
+import numba as nb
+import numpy as np
+import pandas as pd
+from src.utils import get_tick, get_implied_volatility
+from typing import Final, Tuple
+from src.levenberg_marquardt import LevenbergMarquardt
+from scipy import stats as sps
+
+
+_spec_market_params = [
+    ("S", nb.float64),
+    ("r", nb.float64),
+    ("T", nb.float64),
+    ("K", nb.float64[:]),
+    ("C", nb.float64[:]),
+    ("iv", nb.float64[:]),
+    ("types", nb.boolean[:]),
+]
+
+_spec_model_params = [
+    ("alpha", nb.float64),
+    ("v", nb.float64),
+    ("beta", nb.float64),
+    ("rho", nb.float64),
+]
+
+
+@nb.experimental.jitclass(_spec_market_params)
+class MarketParameters(object):
+    S: nb.float64
+    r: nb.float64
+    T: nb.float64
+    K: nb.float64[:]
+    C: nb.float64[:]
+    iv: nb.float64[:]
+    types: nb.boolean[:]
+
+    def __init__(
+        self,
+        S: nb.float64,
+        r: nb.float64,
+        T: nb.float64,
+        K: nb.float64[:],
+        C: nb.float64[:],
+        iv: nb.float64[:],
+        types: nb.boolean[:],
+    ):
+        self.S = S
+        self.r = r
+        self.T = T
+        self.K = K
+        self.C = C
+        self.iv = iv
+        self.types = types
+
+
+@nb.experimental.jitclass(_spec_model_params)
+class ModelParameters(object):
+    alpha: nb.float64
+    v: nb.float64
+    beta: nb.float64
+    rho: nb.float64
+
+    def __init__(
+        self, alpha: nb.float64, v: nb.float64, beta: nb.float64, rho: nb.float64
+    ):
+        self.alpha = alpha
+        self.v = v
+        self.beta = beta
+        self.rho = rho
+
+
+_tmp_values_vol_sabr = {
+    "Fms": nb.types.Array(nb.float64, 1, "A"),
+    "Fm": nb.types.Array(nb.float64, 1, "A"),
+    "q1": nb.types.Array(nb.float64, 1, "A"),
+    "q2": nb.types.Array(nb.float64, 1, "A"),
+    "q3": nb.float64,
+    "S": nb.types.Array(nb.float64, 1, "A"),
+    "zeta": nb.types.Array(nb.float64, 1, "A"),
+    "sqrt": nb.types.Array(nb.float64, 1, "A"),
+    "X": nb.types.Array(nb.float64, 1, "A"),
+    "D": nb.types.Array(nb.float64, 1, "A"),
+    "sig": nb.types.Array(nb.float64, 1, "A"),
+}
+
+# @nb.njit(locals=_tmp_values_vol_sabr)
+def vol_sabr(
+    model: ModelParameters,
+    market: MarketParameters,
+) -> np.array:
+    f, Ks, T = market.S, market.K, market.T
+    alpha, beta, v, rho = model.alpha, model.beta, model.v, model.rho
+    sigmas = []
+    for K in Ks:
+        x = np.log(f / K)
+
+        I_H_1 = (
+            alpha**2 * (K * f) ** (beta - 1) * (1 - beta) ** 2 / 24
+            + alpha * beta * rho * v * (K * f) ** (beta / 2 + - 1/2) / 4
+            + v**2 * (2 - 3 * rho**2) / 24
+        )
+
+        if x == 0.0:
+            I_B_0 = K ** (beta - 1) * alpha
+            sigma = I_B_0 *(1 + I_H_1 * T)
+
+        elif v == 0.0:
+            I_B_0 = alpha * (1 - beta) * x / (-(K ** (1 - beta)) + f ** (1 - beta))
+            sigma = I_B_0 *(1 + I_H_1 * T)
+
+        else:
+            if beta == 1.0:
+                z = v * x / alpha
+
+            elif beta < 1.0:
+                z = v * (f ** (1 - beta) - K ** (1 - beta)) / (alpha * (1 - beta))
+
+            I_B_0 = (
+                v * x / (np.log((np.sqrt(1 - 2 * rho * z + z**2) + z - rho) / (1 - rho)))
+            )
+            sigma = I_B_0 *(1 + I_H_1 * T) 
+
+
+        sigmas.append(sigma)
+    return np.array(sigmas)
+
+
+_tmp_values_jacobian_sabr = {}
+
+# @nb.njit(_signature_jacobian_sabr, locals=_tmp_values_jacobian_sabr)
+def jacobian_sabr(
+    model: ModelParameters,
+    market: MarketParameters,
+):
+    f, Ks, T = market.S, market.K, market.T
+    alpha, beta, v, rho = model.alpha, model.beta, model.v, model.rho
+    ddalphas, ddbeta, ddv, ddrho = [], [], [], []
+    # need a cycle cause may be different formula for different strikes
+    for K in Ks:
+        x = np.log(f / K)
+        I_H = (
+            alpha**2 * (K * f) ** (beta - 1) * (1 - beta) ** 2 / 24
+            + alpha * beta * rho * v * (K * f) ** (beta / 2 - 1 / 2) / 4
+            + v**2 * (2 - 3 * rho**2) / 24
+        )
+        dI_H_alpha = (
+            alpha * (K * f) ** (beta - 1) * (1 - beta) ** 2 / 12
+            + beta * rho * v * (K * f) ** (beta / 2 - 1 / 2) / 4
+        )
+        dI_H_beta = (
+            alpha**2 * (K * f) ** (beta - 1) * (1 - beta) ** 2 * np.log(K * f) / 24
+            + alpha**2 * (K * f) ** (beta - 1) * (2 * beta - 2) / 24
+            + alpha * beta * rho * v * (K * f) ** (beta / 2 + -1 / 2) * np.log(K * f) / 8
+            + alpha * rho * v * (K * f) ** (beta / 2 - 1 / 2) / 4
+        )
+        dI_h_v = (
+            alpha * beta * rho * (K * f) ** (beta / 2 + -1 / 2) / 4
+            + v * (2 - 3 * rho**2) / 12
+        )
+        dI_H_rho = alpha * beta * v * (K * f) ** (beta / 2 + -1 / 2) / 4 - rho * v**2 / 4
+
+        if x == 0.0:
+            I_B = alpha * K ** (beta - 1)
+            B_alpha = K ** (beta - 1)
+            B_beta = K ** (beta - 1) * alpha * np.log(K)
+            B_v = 0.0
+            B_rho = 0.0
+
+        elif v == 0.0:
+            I_B = alpha * (1 - beta) * x / (f ** (1 - beta) - (K ** (1 - beta)))
+            B_alpha = (beta - 1) * x / (K ** (1 - beta) - f ** (1 - beta))
+            B_beta = (
+                alpha
+                * (
+                    K ** (1 - beta)
+                    - f ** (1 - beta)
+                    + (beta - 1)
+                    * (K ** (1 - beta) * np.log(K) - f ** (1 - beta) * np.log(f))
+                )
+                * x
+                / (K ** (1 - beta) - f ** (1 - beta)) ** 2
+            )
+            B_v = 0.0
+            B_rho = 0.0
+
+        elif beta == 1.0:
+            z = v * x / alpha
+            sqrt = np.sqrt(1 - 2 * rho * z + z**2)
+            I_B = v * x / (np.log((sqrt + z - rho) / (1 - rho)))
+            B_alpha = v * x * z / (alpha * sqrt * np.log((rho - z - sqrt) / (rho - 1)) ** 2)
+            B_beta = 0.0
+            B_v = (
+                x
+                * (alpha * sqrt * np.log((rho - z - sqrt) / (rho - 1)) - v * x)
+                / (alpha * sqrt * np.log((rho - z - sqrt) / (rho - 1)) ** 2)
+            )
+            B_rho = (
+                v
+                * x
+                * ((rho - 1) * (z + sqrt) + (-rho + z + sqrt) * sqrt)
+                / (
+                    (rho - 1)
+                    * (-rho + z + sqrt)
+                    * sqrt
+                    * np.log((rho - z - sqrt) / (rho - 1)) ** 2
+                )
+            )
+
+        elif beta < 1.0:
+            z = v * (f ** (1 - beta) - K ** (1 - beta)) / (alpha * (1 - beta))
+            sqrt = np.sqrt(1 - 2 * rho * z + z**2)
+            I_B = v * (-(K ** (1 - beta)) + f ** (1 - beta)) / (alpha * (1 - beta))
+            B_alpha = v * x * z / (alpha * sqrt * np.log((rho - z - sqrt) / (rho - 1)) ** 2)
+            B_beta = (
+                -v
+                * x
+                * (
+                    z / (1 - beta)
+                    + (
+                        -rho * z / (1 - beta)
+                        + z**2 / (1 - beta)
+                        - rho
+                        * v
+                        * (K ** (1 - beta) * np.log(K) - f ** (1 - beta) * np.log(f))
+                        / (alpha * (1 - beta))
+                        + v
+                        * z
+                        * (
+                            2 * K ** (1 - beta) * np.log(K)
+                            - 2 * f ** (1 - beta) * np.log(f)
+                        )
+                        / (2 * alpha * (1 - beta))
+                    )
+                    / sqrt
+                    + v
+                    * (K ** (1 - beta) * np.log(K) - f ** (1 - beta) * np.log(f))
+                    / (alpha * (1 - beta))
+                )
+                / ((-rho + z + sqrt) * np.log((-rho + z + sqrt) / (1 - rho)) ** 2)
+            )
+            B_v = -v * x * ((-rho * z / v + z**2 / v) / sqrt + z / v) / (
+                (-rho + z + sqrt) * np.log((-rho + z + sqrt) / (1 - rho)) ** 2
+            ) + x / np.log((-rho + z + sqrt) / (1 - rho))
+
+            B_rho = (
+                -v
+                * x
+                * (1 - rho)
+                * ((-z / sqrt - 1) / (1 - rho) + (-rho + z + sqrt) / (1 - rho) ** 2)
+                / ((-rho + z + sqrt) * np.log((-rho + z + sqrt) / (1 - rho)) ** 2)
+            )
+
+        sig_alpha = B_alpha*(1 + I_H*T) + dI_H_alpha*I_B*T
+        sig_beta = B_beta*(1 + I_H*T) + dI_H_beta*I_B*T
+        sig_v = B_v*(1 + I_H*T) + dI_h_v*I_B*T
+        sig_rho = B_rho*(1 + I_H*T) + dI_H_rho*I_B*T
+
+        ddalphas.append(sig_alpha)
+        ddbeta.append(sig_beta)
+        ddv.append(sig_v)
+        ddrho.append(sig_rho)
+        # C, vega = black_scholes_vega(K, F, T, r, sig)
+        # return C, vega, sig, sig_alpha, sig_v, sig_beta, sig_rho
+    return np.array(ddalphas),  np.array(ddv), np.array(ddbeta), np.array(ddrho)
+
+
+def calibrate_sabr(
+    df: pd.DataFrame,
+    start_params: np.array,
+    timestamp: int = None,
+    calibration_type: str = "all",
+):
+    """
+    Function to calibrate SABR model.
+    Attributes:
+        @param df (pd.DataFrame): Dataframe with history
+        [
+            timestamp(ns),
+            type(put or call),
+            strike_price(usd),
+            expiration(ns),
+            mark_price(etc/btc),
+            underlying_price(usd)
+        ]
+        @param start_params (np.array): Params to start calibration via LM from
+        @param timestamp (int): On which timestamp to calibrate the model.
+            Should be in range of df timestamps.
+        @param calibration_type(str): Type of calibration. Should be one of: ["all", "beta"]
+
+    Return:
+        calibrated_params (np.array): Array of optimal params on timestamp tick.
+        error (float): Value of error on calibration.
+    """
+    available_calibration_types = ["all", "beta"]
+    if calibration_type not in available_calibration_types:
+        raise ValueError(
+            f"calibration_type should be from {available_calibration_types}"
+        )
+    tick = get_tick(df=df, timestamp=timestamp)
+    iv = []
+    # count ivs
+    for index, t in tick.iterrows():
+        market_iv = get_implied_volatility(
+            option_type=t["type"],
+            C=t["mark_price_usd"],
+            K=t["strike_price"],
+            T=t["tau"],
+            F=t["underlying_price"],
+            r=0.0,
+            error=0.001,
+        )
+        iv.append(market_iv)
+
+    # drop if iv is None
+    tick["iv"] = iv
+    tick = tick[~tick["iv"].isin([None, np.nan])]
+    karr = tick.strike_price.to_numpy(dtype=np.float64)
+    carr = tick.mark_price_usd.to_numpy(dtype=np.float64)
+    iv = tick.iv.to_numpy(dtype=np.float64)
+    T = np.float64(tick.tau.mean())
+    types = np.where(tick["type"] == "call", True, False)
+    # take it zero as on deribit
+    r_val = np.float64(0.0)
+    # tick dataframes may have not similar timestamps -->
+    # not equal value if underlying --> take mean
+    S_val = np.float64(tick.underlying_price.mean())
+    market = MarketParameters(K=karr, T=T, S=S_val, r=r_val, C=carr, types=types, iv=iv)
+
+    def clip_params(sabr_params: np.ndarray) -> np.ndarray:
+        """
+        This funciton project sabr parameters into valid range
+        Attributes:
+            sabr_params(np.ndarray): model parameters
+        Returns:
+            sabr_params(np.ndarray): clipped parameters
+        """
+        eps = 1e-4
+
+        def clip_all(params):
+            alpha, v, beta, rho = params
+            alpha = np.clip(alpha, eps, 50.0)
+            v = np.clip(v, eps, 100.0)
+            # no need there cause by approx formula it can be 1.0
+            beta = np.clip(beta, eps, 1.0)
+            rho = np.clip(rho, -1.0 + eps, 1.0 - eps)
+            sabr_params = np.array([alpha, v, beta, rho])
+            return sabr_params
+
+        if calibration_type == "all":
+            sabr_params = clip_all(sabr_params)
+
+        elif calibration_type == "beta":
+            params = np.concatenate(
+                [
+                    sabr_params[0:2],
+                    np.array([beta]),
+                    sabr_params[2:],
+                ]
+            )
+            sabr_params = clip_all(params)
+            sabr_params = np.concatenate([sabr_params[0:2], sabr_params[3:]])
+
+        return sabr_params
+
+    def get_residuals(sabr_params: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Function calculates residuals of market ivd and calibrated ones
+        and Jacobian matrix
+        Args:
+            sabr_params(np.ndarray): model params
+        Returns:
+            res(np.ndarray) : vector or residuals
+            J(np.ndarray)   : Jacobian
+        """
+        # function supports several calibration types
+        if calibration_type == "all":
+            model_parameters = ModelParameters(
+                sabr_params[0],
+                sabr_params[1],
+                sabr_params[2],
+                sabr_params[3],
+            )
+            J = jacobian_sabr(model=model_parameters, market=market)
+
+        elif calibration_type == "beta":
+            model_parameters = ModelParameters(
+                sabr_params[0], sabr_params[1], beta, sabr_params[2]
+            )
+            J_tmp = jacobian_sabr(model=model_parameters, market=market)
+            J = np.concatenate([J_tmp[0:2], J_tmp[3:]])
+
+        iv = vol_sabr(model=model_parameters, market=market)
+        weights = np.ones_like(market.K)
+        weights = weights / np.sum(weights)
+        res = iv - market.iv
+        return res * weights, J @ np.diag(weights)
+
+    if calibration_type == "all":
+        res = LevenbergMarquardt(500, get_residuals, clip_params, start_params)
+        calibrated_params = np.array(res["x"], dtype=np.float64)
+
+    elif calibration_type == "beta":
+        # beta = 0.9999
+        beta = 1.0
+        res = LevenbergMarquardt(
+            500,
+            get_residuals,
+            clip_params,
+            np.concatenate([start_params[0:2], start_params[3:]]),
+        )
+        calibrated_params = np.array(res["x"], dtype=np.float64)
+        calibrated_params = np.concatenate(
+            [calibrated_params[0:2], np.array([beta]), calibrated_params[2:]]
+        )
+    error = res["objective"][-1]
+    final_params = ModelParameters(
+        calibrated_params[0],
+        calibrated_params[1],
+        calibrated_params[2],
+        calibrated_params[3],
+    )
+    final_vols = vol_sabr(model=final_params, market=market)
+    tick["calibrated_iv"] = final_vols
+    result = tick[
+        [
+            "type",
+            "strike_price",
+            "expiration",
+            "underlying_price",
+            "iv",
+            "calibrated_iv",
+        ]
+    ]
+    result["iv"] = 100 * result["iv"]
+    result["calibrated_iv"] = 100 * result["calibrated_iv"]
+    return calibrated_params, error, result
---
+++ docs/quant/deribit/utils.py
@@ -0,0 +1,359 @@
+import pandas as pd
+from scipy import stats as sps
+import numpy as np
+import datetime
+
+
+def get_tick(df: pd.DataFrame, timestamp: int = None):
+    """Function gets tick for each expiration and strike
+    from closest timestamp from given. If timestamp is None, it takes last one."""
+    if timestamp:
+        data = df[df["timestamp"] <= timestamp].copy()
+        # only not expired on curret tick
+        data = data[data["expiration"] > timestamp].copy()
+    else:
+        data = df.copy()
+        # only not expired on max available tick
+        data = data[data["expiration"] > data["timestamp"].max()].copy()
+    # tau is time before expiration in years
+    data["tau"] = (data.expiration - data.timestamp) / 1e6 / 3600 / 24 / 365
+
+    data_grouped = data.loc[
+        data.groupby(["type", "expiration", "strike_price"])["timestamp"].idxmax()
+    ]
+
+    data_grouped = data_grouped[data_grouped["tau"] > 0.0]
+    # We need Only out of the money to calibrate
+    data_grouped = data_grouped[
+        (
+            (data_grouped["type"] == "call")
+            & (data_grouped["underlying_price"] <= data_grouped["strike_price"])
+        )
+        | (
+            (data_grouped["type"] == "put")
+            & (data_grouped["underlying_price"] >= data_grouped["strike_price"])
+        )
+    ]
+    data_grouped["mark_price_usd"] = (
+        data_grouped["mark_price"] * data_grouped["underlying_price"]
+    )
+    data_grouped = data_grouped[data_grouped["strike_price"] <= 10_000]
+    # print(data_grouped)
+    return data_grouped
+
+# Newton-Raphsen
+def get_implied_volatility(
+    option_type: str,
+    C: float,
+    K: float,
+    T: float,
+    F: float,
+    r: float = 0.0,
+    error: float = 0.001,
+) -> float:
+    """
+    Function to count implied volatility via given params of option, using Newton-Raphson method :
+
+    Args:
+        C (float): Option market price(USD).
+        K (float): Strike(USD).
+        T (float): Time to expiration in years.
+        F (float): Underlying price.
+        r (float): Risk-free rate.
+        error (float): Given threshhold of error.
+
+    Returns:
+        float: Implied volatility in percent.
+    """
+    vol = 1.0
+    dv = error + 1
+    while abs(dv) > error:
+        d1 = (np.log(F / K) + 0.5 * vol**2 * T) / (vol * np.sqrt(T))
+        d2 = d1 - vol * np.sqrt(T)
+        D = np.exp(-r * T)
+        if option_type.lower() == "call":
+            price = F * sps.norm.cdf(d1) - K * sps.norm.cdf(d2) * D
+        elif option_type.lower() == "put":
+            price = -F * sps.norm.cdf(-d1) + K * sps.norm.cdf(-d2) * D
+        else:
+            raise ValueError("Wrong option type, must be 'call' or 'put' ")
+        Vega = F * np.sqrt(T / np.pi / 2) * np.exp(-0.5 * d1**2)
+        PriceError = price - C
+        dv = PriceError / Vega
+        vol = vol - dv
+    return vol
+
+def process_data(data):
+    # only options
+    df = data.copy()
+    df = df[(df["instrument"].str.endswith("C")) | (df["instrument"].str.endswith("P"))].sort_values("dt")
+    df["type"] = np.where(df["instrument"].str.endswith("C"), "call", "put")
+    
+    perpetuals = data[data["instrument"].str.endswith("PERPETUAL")][["dt", "price"]].copy()
+    perpetuals = perpetuals.rename(columns = {"price": "underlying_price"}).sort_values("dt")
+    
+    def get_strike(x):
+        return int(x.split("-")[2])
+    
+    def get_expiration(x):
+        return x.split("-")[1]
+    
+
+    df["strike_price"] = df["instrument"].apply(get_strike)
+    df["expiration"] = df["instrument"].apply(get_expiration)
+    
+    def unix_time_millis(dt):
+        epoch = datetime.datetime.utcfromtimestamp(0)
+        return int((dt - epoch).total_seconds() * 1000_000)
+    
+    def get_normal_date(s):
+        """Function to convert date to find years to maturity"""
+        monthToNum = {
+            "JAN": 1,
+            "FEB": 2,
+            "MAR": 3,
+            "APR": 4,
+            "MAY": 5,
+            "JUN": 6,
+            "JUL": 7,
+            "AUG": 8,
+            "SEP": 9,
+            "OCT": 10,
+            "NOV": 11,
+            "DEC": 12,
+        }
+
+        full_date = s.split("-")[1]
+        try:
+            day = int(full_date[:2])
+            month = monthToNum[full_date[2:5]]
+        except:
+            day = int(full_date[:1])
+            month = monthToNum[full_date[1:4]]
+        
+        year = int("20" + full_date[-2:])
+        exp_date = datetime.datetime(year, month, day)
+        return unix_time_millis(exp_date)
+    
+    df["dt"] = pd.to_datetime(df["dt"])
+    perpetuals["dt"] = pd.to_datetime(perpetuals["dt"])
+    
+    df = pd.merge_asof(df, perpetuals, on="dt",
+                       tolerance=pd.Timedelta('7 minutes'),
+                       direction='nearest',)
+    
+    df["timestamp"] = df["dt"].apply(unix_time_millis)
+    df["expiration"] = df["instrument"].apply(get_normal_date)
+    df = df.rename(columns = {"price": "mark_price"})
+    
+    
+    return df
+
+
+bid_ask_approx = {
+    100: 1.0682687141911174,
+    150: 1.0543091799098152,
+    200: 1.043041829448211,
+    250: 1.0339585615110631,
+    300: 1.0266471731161493,
+    350: 1.0207732598846047,
+    400: 1.0160655324444303,
+    450: 1.012303904194882,
+    500: 1.0093098273690733,
+    550: 1.006938453054222,
+    600: 1.0050722709184021,
+    650: 1.0036159493661156,
+    700: 1.0024921495554828,
+    750: 1.0016381294718064,
+    800: 1.0010029889434058,
+    850: 1.0005454346292013,
+    900: 1.0002319668393267,
+    950: 1.0000354085726098,
+    1000: 1.0,
+    1050: 1.0,
+    1100: 1.0,
+    1150: 1.0000351723918721,
+    1200: 1.0001647525653266,
+    1250: 1.0003277282334841,
+    1300: 1.0005178140988367,
+    1350: 1.0007299111473158,
+    1400: 1.0009598827508623,
+    1450: 1.0012043730280786,
+    1500: 1.0014606594872277,
+    1550: 1.0017265334811813,
+    1600: 1.002000203225129,
+    1650: 1.0022802151185861,
+    1700: 1.0025653899169722,
+    1750: 1.0028547709500737,
+    1800: 1.003147582113679,
+    1850: 1.003443193789814,
+    1900: 1.0037410951991417,
+    1950: 1.0040408719715295,
+    2000: 1.0043421879499168,
+    2050: 1.0046447704284949,
+    2100: 1.0049483981770093,
+    2150: 1.00525289172534,
+    2200: 1.005558105481755,
+    2250: 1.0058639213387546,
+    2300: 1.0061702434857376,
+    2350: 1.006476994200719,
+    2400: 1.0067841104363124,
+    2450: 1.0070915410500683,
+    2500: 1.0073992445575557,
+    2550: 1.0077071873095236,
+    2600: 1.0080153420131015,
+    2650: 1.0083236865321064,
+    2700: 1.00863220291378,
+    2750: 1.0089408765992163,
+    2800: 1.0092496957828119,
+    2850: 1.009558650892614,
+    2900: 1.0098677341687459,
+    2950: 1.0101769393214,
+    3000: 1.0104862612533803,
+    3050: 1.0107956958350135,
+    3100: 1.0111052397215405,
+    3150: 1.011414890204975,
+    3200: 1.0117246450939223,
+    3250: 1.0120345026160786,
+    3300: 1.0123444613391364,
+    3350: 1.0126545201066126,
+    3400: 1.0129646779857948,
+    3450: 1.0132749342255074,
+    3500: 1.0135852882218488,
+    3550: 1.0138957394903998,
+    3600: 1.0142062876436715,
+    3650: 1.0145169323728145,
+    3700: 1.0148276734327777,
+    3750: 1.0151385106302677,
+    3800: 1.0154494438139845,
+    3850: 1.0157604728666931,
+    3900: 1.0160715976988008,
+    3950: 1.0163828182431383,
+    4000: 1.0166941344507299,
+    4050: 1.017005546287362,
+    4100: 1.0173170537307983,
+    4150: 1.0176286567685209,
+    4200: 1.017940355395898,
+    4250: 1.0182521496146968,
+    4300: 1.0185640394318767,
+    4350: 1.0188760248586137,
+    4400: 1.0191881059095027,
+    4450: 1.019500282601917,
+    4500: 1.0198125549554835,
+    4550: 1.0201249229916602,
+    4600: 1.0204373867333918,
+    4650: 1.020749946204831,
+    4700: 1.0210626014311126,
+    4750: 1.021375352438168,
+    4800: 1.0216881992525795,
+    4850: 1.022001141901457,
+    4900: 1.0223141804123401,
+    4950: 1.0226273148131204,
+    5000: 1.0229405451319749,
+    5050: 1.0232538713973145,
+    5100: 1.0235672936377422,
+    5150: 1.0238808118820173,
+    5200: 1.0241944261590292,
+    5250: 1.024508136497773,
+    5300: 1.024821942927332,
+    5350: 1.0251358454768627,
+    5400: 1.0254498441755824,
+    5450: 1.02576393905276,
+    5500: 1.0260781301377078,
+    5550: 1.0263924174597738,
+    5600: 1.0267068010483393,
+    5650: 1.0270212809328116,
+    5700: 1.027335857142622,
+    5750: 1.0276505297072238,
+    5800: 1.0279652986560879,
+    5850: 1.0282801640187025,
+    5900: 1.0285951258245716,
+    5950: 1.0289101841032133,
+    6000: 1.0292253388841592,
+    6050: 1.029540590196953,
+    6100: 1.0298559380711505,
+    6150: 1.0301713825363183,
+    6200: 1.0304869236220346,
+    6250: 1.030802561357888,
+    6300: 1.0311182957734768,
+    6350: 1.0314341268984102,
+    6400: 1.0317500547623064,
+    6450: 1.0320660793947938,
+    6500: 1.0323822008255106,
+    6550: 1.0326984190841038,
+    6600: 1.033014734200231,
+    6650: 1.0333311462035577,
+    6700: 1.03364765512376,
+    6750: 1.0339642609905217,
+    6800: 1.0342809638335382,
+    6850: 1.034597763682512,
+    6900: 1.0349146605671555,
+    6950: 1.0352316545171907,
+    7000: 1.0355487455623482,
+    7050: 1.0358659337323681,
+    7100: 1.0361832190569993,
+    7150: 1.0365006015660005,
+    7200: 1.0368180812891388,
+    7250: 1.0371356582561906,
+    7300: 1.0374533324969417,
+    7350: 1.0377711040411872,
+    7400: 1.0380889729187308,
+    7450: 1.038406939159386,
+    7500: 1.0387250027929744,
+    7550: 1.0390431638493276,
+    7600: 1.0393614223582865,
+    7650: 1.039679778349701,
+    7700: 1.0399982318534284,
+    7750: 1.0403167828993385,
+    7800: 1.0406354315173074,
+    7850: 1.040954177737222,
+    7900: 1.0412730215889767,
+    7950: 1.0415919631024773,
+    8000: 1.0419110023076368,
+    8050: 1.0422301392343782,
+    8100: 1.0425493739126337,
+    8150: 1.0428687063723447,
+    8200: 1.0431881366434614,
+    8250: 1.0435076647559436,
+    8300: 1.0438272907397597,
+    8350: 1.0441470146248877,
+    8400: 1.0444668364413152,
+    8450: 1.0447867562190383,
+    8500: 1.0451067739880622,
+    8550: 1.045426889778402,
+    8600: 1.0457471036200812,
+    8650: 1.0460674155431333,
+    8700: 1.0463878255776002,
+    8750: 1.0467083337535337,
+    8800: 1.0470289401009945,
+    8850: 1.047349644650052,
+    8900: 1.047670447430786,
+    8950: 1.0479913484732843,
+    9000: 1.0483123478076446,
+    9050: 1.0486334454639736,
+    9100: 1.0489546414723878,
+    9150: 1.0492759358630117,
+    9200: 1.04959732866598,
+    9250: 1.0499188199114364,
+    9300: 1.0502404096295337,
+    9350: 1.0505620978504344,
+    9400: 1.0508838846043091,
+    9450: 1.0512057699213393,
+    9500: 1.0515277538317143,
+    9550: 1.0518498363656332,
+    9600: 1.0521720175533045,
+    9650: 1.052494297424946,
+    9700: 1.0528166760107844,
+    9750: 1.0531391533410555,
+    9800: 1.053461729446005,
+    9850: 1.0537844043558875,
+    9900: 1.054107178100967,
+    9950: 1.0544300507115165,
+}
+
+
+def get_bid_ask(strike):
+    return bid_ask_approx[strike]
+
+def round_params(params, n_signs = 3):
+    return [round(x, n_signs) for x in params]
