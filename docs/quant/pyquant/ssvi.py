5050aee34aaf3e9bd0d5db9a09335426bf458972
Dmitriy Bazanov <zugzvangg1@gmail.com>
2024-12-01 16:55:05 +0300

    fix: init delta space quotes for all the surface

--- docs/quant/pyquant/ssvi.py
+++ docs/quant/pyquant/ssvi.py
@@ -1,11 +1,65 @@
-import numpy as np
 import numba as nb
+import numpy as np
 
-from .common import *
 from .black_scholes import *
+from .common import *
+from .svi import *
 from .vol_surface import *
 
-from .svi import *
 
 class SSVI:
-    pass
+    def __init__(
+        self, vol_slime_chain_spaces: list[VolSmileChainSpace], is_log: bool = False
+    ) -> None:
+        self.is_log = is_log
+        self.delta_space_params_list = []
+        self.vol_slime_chain_spaces = vol_slime_chain_spaces
+        for vol_slime_chain_space in self.vol_slime_chain_spaces:
+            if self.is_log:
+                print(
+                    f"===== Get natural params for tau = {vol_slime_chain_space.T} ===== "
+                )
+                print(f"Market IV {vol_slime_chain_space.sigmas}")
+            # for every time to maturity calibrate it's own SVI with raw params
+            svi_calc = SVICalc()
+            svi_calibrated_params, svi_error = svi_calc.calibrate(
+                vol_slime_chain_space,
+                CalibrationWeights(np.ones(len(vol_slime_chain_space.Ks))),
+                False,
+                False,
+            )
+            if self.is_log:
+                print(
+                    f"Calibrated to market. Error = {svi_error.v}. Raw params = {svi_calibrated_params.array()}"
+                )
+
+                svi_test_iv = svi_calc.implied_vols(
+                    vol_slime_chain_space.forward(),
+                    Strikes(vol_slime_chain_space.Ks),
+                    svi_calibrated_params,
+                )
+
+                print(f"Calibrated IV {svi_test_iv.data}")
+
+            # Now get delta-space quotes
+            svi_delta_space_chain = svi_calc.delta_space(
+                vol_slime_chain_space.forward(),
+                svi_calibrated_params,
+            ).to_chain_space()
+            if self.is_log:
+                print(f"Delta-space strikes: {svi_delta_space_chain.strikes().data}")
+
+            # Calibrate to delta-space
+            svi_calibrated_params_delta, __ = svi_calc.calibrate(
+                svi_delta_space_chain, CalibrationWeights(np.ones(5)), False, True
+            )
+            if self.is_log:
+                print("Delta space params:", svi_calibrated_params_delta.array())
+                svi_test_iv_delta = svi_calc.implied_vols(
+                    vol_slime_chain_space.forward(),
+                    Strikes(vol_slime_chain_space.Ks),
+                    svi_calibrated_params_delta,
+                )
+                print(f"Delta-space params market IV: {svi_test_iv_delta.data}")
+
+            self.delta_space_params_list.append(svi_calibrated_params_delta.array())
--- docs/quant/ssvi_example.py
+++ docs/quant/ssvi_example.py
@@ -1,2 +1,38 @@
+import numpy as np
+from pyquant.black_scholes import *
+from pyquant.common import *
 from pyquant.ssvi import SSVI
-print(1)
+from pyquant.svi import SVICalc
+from pyquant.vol_surface import *
+
+strikes = np.array(
+    [1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0]
+)
+F = 1723.75
+pvs = np.array(
+    [
+        1.72375,
+        1.72375,
+        3.4475,
+        6.895,
+        26.718125,
+        11.204375,
+        4.309375,
+        1.72375,
+        0.861875,
+        0.861875,
+    ]
+)
+
+vol_smile_chains = []
+for tau in [0.01, 0.1, 1]:
+    forward = Forward(Spot(F), ForwardYield(0.01), TimeToMaturity(tau))
+    bs_calc = BSCalc()
+    implied_vols = bs_calc.implied_vols(forward, Strikes(strikes), Premiums(pvs)).data
+
+    vol_smile_chain = VolSmileChainSpace(
+        forward, Strikes(strikes), ImpliedVols(implied_vols)
+    )
+    vol_smile_chains.append(vol_smile_chain)
+
+print(SSVI(vol_smile_chains, is_log=True).delta_space_params_list)
