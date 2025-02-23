f38d723bc0389793f7975fe64f259d25328d2be1
Dmitriy Bazanov <zugzvangg1@gmail.com>
2025-02-23 16:25:46 +0300

    vol_surface inited

--- docs/quant/ssvi_example.py
+++ docs/quant/ssvi_example.py
@@ -23,25 +23,27 @@ premiums = np.array(
     ]
 )
 spot = 1723.75
-ttms = np.array([0.03 for _ in range(len(strikes))])
-forward_rates = np.array([spot + 3 for _ in range(len(strikes))])
-# forward_yields = np.array([0.01 for _ in range(len(strikes))])
-# discount_yields = np.array([0.02 for _ in range(len(strikes))])
+ttms = np.array([0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1])
+
+forward_rates = np.array(
+    [1700.0, 1702.0, 1703.0, 1704.0, 1705.0, 1706.0, 1707.0, 1708.0, 1709.0, 1710.0]
+)
 
 forward_curve: ForwardCurve = forward_curve_from_forward_rates(
     Spot(spot), ForwardRates(forward_rates), TimesToMaturity(ttms)
 )
-print(forward_curve)
 
-# vol_surface_chain_space = VolSurfaceChainSpace(
-#     forward_curve=forward_curve,
-#     times_to_maturity=TimesToMaturity(ttms),
-#     strikes=Strikes(strikes),
-#     option_types=OptionTypes(np.array([True if x <= spot else False for x in strikes])),
-#     premiums=Premiums(premiums),
-#     compute_implied_vol=True,
-# )
-# # преобразуем в delta-space
+
+vol_surface_chain_space = VolSurfaceChainSpace(
+    forward_curve=forward_curve,
+    times_to_maturity=TimesToMaturity(ttms),
+    strikes=Strikes(strikes),
+    option_types=OptionTypes(np.array([True if x <= spot else False for x in strikes])),
+    premiums=Premiums(premiums),
+    compute_implied_vol=True,
+)
+print(vol_surface_chain_space)
+# преобразуем в delta-space
 # vol_surface_delta_space = SVICalc().surface_to_delta_space(vol_surface_chain_space)
 
 # ssvi = SSVICalc(is_log=True)
