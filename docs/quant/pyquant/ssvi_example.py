correct SSVI formula

docs/quant/pyquant/ssvi_example.py
@@ -1,9 +1,10 @@
+from .black_scholes import *
+from .common import *
+from .ssvi import SSVICalc
+from .svi import SVICalc
+from .vol_surface import *
+
 import numpy as np
-from pyquant.black_scholes import *
-from pyquant.common import *
-from pyquant.ssvi import SSVICalc
-from pyquant.svi import SVICalc
-from pyquant.vol_surface import *
 
 strikes = np.array(
     [1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0, 2000.0, 2100.0, 2200.0]

docs/quant/pyquant/wasc_example.py
@@ -1,8 +1,9 @@
+from .black_scholes import *
+from .common import *
+from .vol_surface import *
+from .wasc import WASC
+
 import numpy as np
-from black_scholes import *
-from common import *
-from vol_surface import *
-from wasc import WASC
 
 strikes = np.array([1300.0, 1400, 1500, 1600, 1700, 1800.0, 1900, 2000, 2100, 2200])
 tau = 0.01